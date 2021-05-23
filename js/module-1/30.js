b5cd847c0f129aac4668bf14029645f17049b4f7
ITgrand <vitaliidovhopolyk@gmail.com>
2021-05-24 02:15:50 +0300

    д/з

---
+++ js/module-1/30.js
@@ -0,0 +1,17 @@
+//Функция getNameLength(name) принимает имя(параметр name) и возвращает строку, в которой указана его длина.
+//Дополни шаблонную строку в переменной message длиной строки из параметра name.
+
+
+//function getNameLength(name) {
+//  const message = `Name ${} is ${} characters long`; // Change this line
+//
+  //return message;
+//}
+//
+
+
+function getNameLength(name) {
+  const message = `Name ${name} is ${name.length} characters long`;
+
+  return message;
+}
