da60a5117ab121f9f52403831474dde85c211f9e
ITgrand <vitaliidovhopolyk@gmail.com>
2021-06-06 18:33:53 +0300

    hw10

---
+++ js/module-3/5.js
@@ -0,0 +1,42 @@
+
+// Дополни код присвоив объявленным переменным выражения обращения к соответствующим свойствам обьекта apartment используя синтаксис «квадратных скобок».
+
+// aptRating - рейтинг;
+// aptDescr - описание;
+// aptPrice - цена;
+// aptTags - теги.
+
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+// };
+
+// // Change code below this line
+// const aptRating = apartment;
+// const aptDescr = apartment;
+// const aptPrice = apartment;
+// const aptTags = apartment;
+// // Change code above this line
+
+
+const apartment = {
+  imgUrl: "https://via.placeholder.com/640x480",
+  descr: "Spacious apartment in the city center",
+  rating: 4,
+  price: 2153,
+  tags: ["premium", "promoted", "top"],
+};
+
+// Change code below this line
+const aptRating = apartment["rating"];
+const aptDescr = apartment["descr"];
+const aptPrice = apartment["price"];
+const aptTags = apartment["tags"];
+// Change code above this line
+
+
+console.log(aptRating);
---
+++ js/module-3/9.js
@@ -0,0 +1,37 @@
+
+
+
+// Дополни код объявления объекта credentials так, чтобы в результате у него были два свойства: email и password,
+//     имена которых хранятся в переменных emailInputName и passwordInputName.
+
+// Значением свойства email должна быть строка "henry.carter@aptmail.com", а значением свойства password - строка "jqueryismyjam".
+
+// const emailInputName = 'email';
+// const passwordInputName = 'password';
+
+// const credentials = {
+//   // Change code below this line
+  
+  
+  
+//   // Change code above this line
+// };
+
+
+const emailInputName = 'email';
+const passwordInputName = 'password';
+
+const credentials = {
+  // Change code below this line
+    [emailInputName]: "henry.carter@aptmail.com",
+    [passwordInputName]: "jqueryismyjam"
+  
+  
+  
+  // Change code above this line
+};
+
+console.log(credentials.email);
+console.log(credentials.password);
+// console.log(password);
+
